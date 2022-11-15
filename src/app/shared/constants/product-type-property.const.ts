dfd6bf20acb9bc501cd27830be79b62a0a9b8dd7
zidiks <zidiks228@gmail.com>
2022-11-15 14:10:43 +0300

    upd: fix properties descriptions

--- src/app/shared/constants/product-type-property.const.ts
+++ src/app/shared/constants/product-type-property.const.ts
@@ -4,30 +4,30 @@ import { ProductTypePropertyDataModel } from "../models/product-type-property-da
 export const productsTypesPropertiesData: Record<ProductTypePropertyType, ProductTypePropertyDataModel> = {
   [ProductTypePropertyType.StringMultiSelect]: {
     name: 'Множественный строковый выбор',
-    description: 'Можно задать для продукта несколько строковых значений из заранее созданного списка',
+    description: 'Можно задать для свойства несколько строковых значений из заранее созданного списка',
   },
   [ProductTypePropertyType.StringSelect]: {
     name: 'Строковый выбор',
-    description: 'Можно задать для продукта одно строковое значение из заранее созданного списка',
+    description: 'Можно задать для свойства одно строковое значение из заранее созданного списка',
   },
   [ProductTypePropertyType.StringInput]: {
     name: 'Произвольная строка',
-    description: 'Можно задать для продукта произвольное строковое значение',
+    description: 'Можно задать для свойства произвольное строковое значение',
   },
   [ProductTypePropertyType.NumberInput]: {
     name: 'Произвольное число',
-    description: 'Можно задать для продукта произвольное числовое значение',
+    description: 'Можно задать для свойства произвольное числовое значение',
   },
   [ProductTypePropertyType.NumberSelect]: {
     name: 'Числовой выбор',
-    description: 'Можно задать для продукта одно числовое значение из заранее созданного списка',
+    description: 'Можно задать для свойства одно числовое значение из заранее созданного списка',
   },
   [ProductTypePropertyType.CheckBox]: {
     name: 'Флаг',
-    description: 'Можно задать для продукта одно логическое значение (верно/неверно)',
+    description: 'Можно задать для свойства одно логическое значение (верно/неверно)',
   },
   [ProductTypePropertyType.Color]: {
     name: 'Цвет',
-    description: 'Можно задать для продукта цвет с помощью специального диалогового окна',
+    description: 'Можно задать для свойства цвет с помощью специального диалогового окна',
   },
 }
